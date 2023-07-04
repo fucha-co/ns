a7d0a36326f102ab17691a76b0b8004572e70ba7
FUCHA <nommm@pm.me>
2023-07-04 10:12:50 +1000

    ~

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,20 @@ import { defineConfig } from 'astro/config'
 import compress from 'astro-compress'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
+import sharp from 'sharp'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind()],
-  //experimental: { assets: true }, // // astro.build/blog/images/
+  integrations: [
+    compress(),
+    mdx(),
+    tailwind(),
+    {
+      transformer: sharp,
+      name: 'sharp',
+    },
+  ],
+  //experimental: { assets: true }, // // docs.astro.build/en/guides/assets/ & astro.build/blog/images/
 })
+
+
